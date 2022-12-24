4ac1201d7960ffb7b4c778e5da58bf11ab68221b
syui <syui@syui.cf>
2022-12-25 00:32:32 +0900

    fix oneline

--- scpt/vimdoc.zsh
+++ scpt/vimdoc.zsh
@@ -12,14 +12,14 @@ fi
 
 case $OSTYPE in
 	darwin*)
-		a=`grep -Rn "*$1*" $f|cut -d : -f 2`
+		a=`grep -Rn "*$1*" $f|cut -d : -f 2|head -n 1`
 		;;
 	linux*)
-		a=`grep -Rn "*$1*" $f|cut -d : -f 1`
+		a=`grep -Rn "*$1*" $f|cut -d : -f 1|head -n 1`
 		;;
 esac
 
 b=$(($a + 500))
 tmp=`awk "NR==$a,NR==$b" $f`
-b=`echo "$tmp"|grep -n "^[a-z]"|awk "NR==2"|cut -d : -f 1`
+b=`echo "$tmp"|grep -n "^[a-z]"|awk "NR==2"|cut -d : -f 1|head -n 1`
 echo "$tmp"|awk "NR==1,NR==$b"|sed -e '$d' 
